d9e1f0908fcdb6744470be1f01b8d396c69a7c67
javi11 <javicuba11@gmail.com>
2022-03-22 15:00:22 +0100

    build: creating expandible menu

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -2,22 +2,25 @@ import 'dart:math';
 
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:timer_f1/app/modules/flight_history/widgets/history.dart';
+import 'package:timer_f1/app/modules/home/widgets/expandible_menu.dart';
 import 'package:timer_f1/app/modules/program/widgets/program-list.dart';
 import 'package:timer_f1/app/modules/usb_device/controllers/usb_serial_controller.dart';
 import 'package:timer_f1/app/routes/app_pages.dart';
 import 'package:timer_f1/global_widgets/device_status.dart';
 import 'package:timer_f1/global_widgets/drawer.dart';
-import 'package:timer_f1/global_widgets/clipped_parts.dart';
-
-const appBarWithFiltersSize = 350.0;
 
 class HomePage extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     var isUsbConnected =
         ref.watch(usbControllerProvider.select((value) => value.isConnected));
     var startFlight = useCallback(
@@ -27,16 +30,9 @@ class HomePage extends HookConsumerWidget {
                 '${Routes.BLUETOOTH}?redirectTo=/${Routes.FLIGHT_TRACKER}'),
         [isUsbConnected]);
     var tabController = useTabController(initialLength: 3, initialIndex: 0);
-    var openFilters = useState(true);
-    var controller = useAnimationController(
+    var expandibleMenuController = useAnimationController(
       duration: const Duration(milliseconds: 200),
     );
-    var clipW = useListenable(Tween<double>(
-      begin: 0,
-      end: appBarWithFiltersSize,
-    ).animate(
-      controller,
-    ));
 
     return Scaffold(
       drawer: CustomDrawer(
@@ -51,10 +47,10 @@ class HomePage extends HookConsumerWidget {
           IconButton(
             icon: Icon(Icons.filter_alt_outlined),
             onPressed: () {
-              if (controller.isCompleted) {
-                controller.reverse();
+              if (expandibleMenuController.isCompleted) {
+                expandibleMenuController.reverse();
               } else {
-                controller.forward();
+                expandibleMenuController.forward();
               }
             },
           )
@@ -93,34 +89,14 @@ class HomePage extends HookConsumerWidget {
       body: TabBarView(
         controller: tabController,
         children: [
-          ClippedPartsWidget(
-            top: Container(
-              width: MediaQuery.of(context).size.width,
-              height: 150 + clipW.value,
-              color: Colors.blue[400],
-            ),
-            bottom: Stack(children: <Widget>[
-              Container(
-                height: 190 + clipW.value,
-                color: clipW.value > 100
-                    ? Colors.black.withOpacity(
-                        0 + (clipW.value / (appBarWithFiltersSize - 500)))
-                    : Colors.blue[100],
-              ),
-              Container(
-                  color: Colors.black.withOpacity(
-                      0 + (clipW.value / (appBarWithFiltersSize - 100))),
-                  child: History(onStartFlight: startFlight))
-            ]),
-            splitFunction: (Size size, double x) {
-              // normalizing x to make it exactly one wave
-              final normalizedX = x / size.width * 3 * pi;
-              final waveHeight = size.height / 40;
-              final y = size.height / 14 - sin(cos(normalizedX)) * waveHeight;
-
-              return y + clipW.value;
-            },
-          ),
+          Stack(children: <Widget>[
+            Container(child: History(onStartFlight: startFlight)),
+            ExpandibleMenu(
+              actions: Container(),
+              expandedActions: Container(),
+              controller: expandibleMenuController,
+            )
+          ]),
           SizedBox(
             width: 10,
           ),
